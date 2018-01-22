4d2091aa476a2ea40c8f2fa7d229eb22b631b606
theodac <32874437+theodac@users.noreply.github.com>
2018-01-22 17:18:51 +0100

    Ajout categories

--- include/accueil.inc.php
+++ include/accueil.inc.php
@@ -13,12 +13,19 @@ try{
 catch (PDOException $e){
     echo ($e -> getMessage());
 }
-$sql = "SELECT * FROM `t_articles` ORDER BY `ARTDATE` DESC LIMIT 0,5";
+
+$sql ="SELECT * FROM t_articles LEFT JOIN t_categories_has_t_articles
+ ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_ARTICLES_ID_ARTICLE LEFT JOIN t_categories ON t_categories_has_t_articles.T_CATEGORIES_ID_CATEGORIE=t_categories.ID_CATEGORIE";
+
+
 $reponse = $db ->query($sql);
 
 
 while ($donnees= $reponse->fetch(PDO::FETCH_ASSOC)){
 
-    echo (html_entity_decode( "<div>"."<br/>" . "<h2>".$donnees['ARTTITRE'] . "</h2>". "<br/>" . "<h3>".  $donnees['ARTCHAPO'] ."</h3>". "<br/>" . "<div>". $donnees['ARTCONTENU'] ."</div>" . "<br/>" . "</div>" .  "<hr/>"));
+    echo (html_entity_decode( "<div>"."<br/>" . "<h2>".$donnees['ARTTITRE'] . "</h2>". "<br/>"
+        . "<h3>".  $donnees['ARTCHAPO'] ."</h3>". "<br/>"
+        . "<div>". $donnees['ARTCONTENU'] ."</div>" . "<br/>"
+        . "</div>" . $donnees['CATLIBELLE']."<hr/>"));
 
 }
