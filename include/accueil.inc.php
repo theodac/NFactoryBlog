16503a158384008e1cde46935c56581720dece85
theodac <32874437+theodac@users.noreply.github.com>
2018-01-25 12:11:51 +0100

    Ajout pagination fonctionnel

--- include/accueil.inc.php
+++ include/accueil.inc.php
@@ -5,8 +5,8 @@ $retourTotale = $db->query("SELECT COUNT(*) AS total FROM T_ARTICLES");
 $donneesTotale = $retourTotale->fetch(PDO::FETCH_ASSOC);
 $totale = $donneesTotale['total'];
 $nombreDePage = ceil($totale/$msgParPage);
-if (isset($_GET['page'])){
-    $pageActuelle = intval($_GET['page']);
+if (isset($_GET['debut'])){
+    $pageActuelle = intval($_GET['debut']);
     if ($pageActuelle > $nombreDePage){
         $pageActuelle = $nombreDePage;
     }
@@ -14,30 +14,35 @@ if (isset($_GET['page'])){
     $pageActuelle = 1;
 }
 
+$premiereEntree = ($pageActuelle-1) *$msgParPage; // On calcul la première entrée à lire
+$sql ="SELECT * FROM t_articles 
 
-$sql ="SELECT * FROM t_articles LEFT JOIN t_categories_has_t_articles
- ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_ARTICLES_ID_ARTICLE LEFT JOIN t_categories ON t_categories_has_t_articles.T_CATEGORIES_ID_CATEGORIE=t_categories.ID_CATEGORIE";
-$reponse = $db ->query($sql);
-$premiereEntree=($pageActuelle-1)*$msgParPage; // On calcul la première entrée à lire
+LEFT JOIN t_categories_has_t_articles ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_ARTICLES_ID_ARTICLE 
+LEFT JOIN t_categories ON t_categories_has_t_articles.T_CATEGORIES_ID_CATEGORIE=t_categories.ID_CATEGORIE
+ORDER BY ID_ARTICLE DESC LIMIT   $premiereEntree,   $msgParPage   
+";
+
+$reponse = $db -> query($sql);
+
 
 // La requête sql pour récupérer les messages de la page actuelle.
-$reponsee=$db->query('SELECT * FROM t_articles ORDER BY ID_ARTICLE DESC LIMIT '.$premiereEntree.', '.$msgParPage.'');
+//$reponse=$db->query('SELECT * FROM t_articles ORDER BY ID_ARTICLE DESC LIMIT '.$premiereEntree.', '.$msgParPage.'');
 
 
 
-while ($donnees= $reponse->fetch(PDO::FETCH_ASSOC)){
+while ($donnees = $reponse -> fetch(PDO::FETCH_ASSOC)){
 
-    echo (html_entity_decode( "<div>"."<br/>" . "<h2>".$donnees['ARTTITRE'] . "</h2>". "<br/>"
+    echo (html_entity_decode( "<div>"."<br/>" . "<h2>". $donnees['ARTTITRE'] . "</h2>". "<br/>"
         . "<h3>".  $donnees['ARTCHAPO'] ."</h3>". "<br/>"
         . "<div>". $donnees['ARTCONTENU'] ."</div>" . "<br/>"
         . "</div>" . $donnees['CATLIBELLE']."<hr/>"));
 
 }
-echo '<p align="center">Page : ';
+echo '<p align ="center">Page : ';
 for($i=1; $i<=$nombreDePage; $i++)
 {
     //On va faire notre condition
-    if($i==$pageActuelle)
+    if($i == $pageActuelle)
     {
         echo ' [ '.$i.' ] ';
     }
